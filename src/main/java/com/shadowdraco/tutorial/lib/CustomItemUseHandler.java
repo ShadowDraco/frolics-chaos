3b7d60942b850e7b277e11e0364bd328fa207d15
Ethan Storm <stormethan10@gmail.com>
2023-02-25 20:00:50 -0800

    new custom item handler overrides the ruby item handler, so I implemented it there

--- src/main/java/com/shadowdraco/tutorial/lib/CustomItemUseHandler.java
+++ src/main/java/com/shadowdraco/tutorial/lib/CustomItemUseHandler.java
@@ -1,6 +1,7 @@
 package com.shadowdraco.tutorial.lib;
 
 import com.shadowdraco.tutorial.registry.ModEnchantments;
+import com.shadowdraco.tutorial.registry.ModItems;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EquipmentSlot;
 
@@ -14,7 +15,9 @@ import net.minecraft.world.World;
 public class CustomItemUseHandler {
 
     public static Boolean AfkBlocking = false;
+    public static Boolean charged = false;
     AFKBlocker afkBlocker;
+    DoubleJumper doubleJumper;
     public CustomItemUseHandler() {
         System.out.println("\n--DTM created custom item use handler !---");
     }
@@ -26,8 +29,7 @@ public class CustomItemUseHandler {
         if (!world.isClient) {
             // if the item used has the afk mine enchant
             checkAfkBlocker(world, player, usedStack);
-
-
+            checkDoubleJumper(world, player, usedStack);
         }
         return TypedActionResult.success(usedStack);
     }
@@ -35,7 +37,7 @@ public class CustomItemUseHandler {
     public void checkAfkBlocker(World world, PlayerEntity player, ItemStack usedStack) {
         if (EnchantmentHelper.getLevel(ModEnchantments.AFK_MINE_ENCHANTMENT, usedStack) > 0) {
 
-            if (AfkBlocking) {
+            if (!AfkBlocking) {
                 if (player.getY() < 30) {
 
                     System.out.println("AfkBlocking Started");
@@ -57,6 +59,38 @@ public class CustomItemUseHandler {
                 player.setInvisible(false);
                 player.sendMessage(Text.literal("Welcome back!"));
             }
+        }
+    }
+
+
+
+    public void checkDoubleJumper(World world, PlayerEntity user, ItemStack usedStack) {
+        // if using a ruby item
+        if (usedStack.isItemEqual(ModItems.RUBY.getDefaultStack())) {
+            // change the charge
+            charged = !charged;
+            // alert the user
+            if (charged) {
+                user.sendMessage(Text.literal("Charged!"));
+                user.setGlowing(true);
+            } else {
+                user.sendMessage(Text.literal("UnCharged!"));
+                user.setGlowing(false);
+            }
+            // toggle the double jumper thread
+            toggleDoubleJump(world, user);
+        }
+    }
+
+    public void toggleDoubleJump(World world, PlayerEntity user) {
+        if (charged) {
+            System.out.println("Thread started");
+            doubleJumper = new DoubleJumper(world, user);
+            doubleJumper.start();
+        } else {
+            System.out.println("Thread interrupted");
+            doubleJumper.deleteBlocks();
+            doubleJumper.interrupt();
         }
     }
 
